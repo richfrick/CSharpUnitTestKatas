0fe3fa8fd2a26038520b6eebee1fa78ab530c2f5
richardfrickleton <richard.frickleton@hotmail.com>
2025-09-26 10:24:56 +0100

    kata for adding <lineNumber>: to entries in a list

---
+++ CSharpUnitTestKatas/DailyKatas/LineNumbering.cs
@@ -0,0 +1,13 @@
+namespace CSharpUnitTestKatas;
+
+public class LineNumbering
+{
+    public static List<string> Number(List<string> lines)
+    {
+        for (int i = 0; i < lines.Count; i++)
+        {
+            lines[i] = $"{i + 1}: {lines[i]}";
+        }
+        return lines;
+    }
+}
